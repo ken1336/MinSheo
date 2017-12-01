7ee0d18f5bb653f2ba3c0d01c0f96858a3980705
min <ken1336@naver.com>
2017-12-01 20:48:47 +0900

    commit1

--- app/src/main/java/applicaton/android/com/sehonmin/MainActivity.java
+++ app/src/main/java/applicaton/android/com/sehonmin/MainActivity.java
@@ -1,15 +1,74 @@
 package applicaton.android.com.sehonmin;
 
+import android.app.Activity;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import applicaton.android.com.sehonmin.usermanagement.core.User;
 
 public class MainActivity extends AppCompatActivity {
 
-    //start
-    
+    private final static String TAG="debug from min";
+    private Button login_btn;
+    private EditText edit_id;
+    private EditText edit_password;
+    private Button signup_btn;
+    private User user;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login_activity);
+        login_btn=(Button)findViewById(R.id._login);
+        edit_id=(EditText)findViewById(R.id._id);
+        edit_password=(EditText)findViewById(R.id._password);
+        signup_btn=(Button)findViewById(R.id._signup);
+        user=User.getInstance(this);
+
+
+
+
+
+        /*mAuthListener = new FirebaseAuth.AuthStateListener() { // 인증 상태 리스너
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Log.d("1", "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    Log.d("1", "onAuthStateChanged:signed_out");
+                }
+            }
+        };*/
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               if(user.logIn(edit_id.getText().toString(), edit_password.getText().toString())){
+                   Toast.makeText(MainActivity.this, "hello", Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
+        });
+
+        signup_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                user.signUp(edit_id.getText().toString(), edit_password.getText().toString());
+            }
+        });
+
     }
+
+
+
+
+
+
 }
---
+++ app/src/main/java/applicaton/android/com/sehonmin/usermanagement/core/User.java
@@ -0,0 +1,96 @@
+package applicaton.android.com.sehonmin.usermanagement.core;
+
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import applicaton.android.com.sehonmin.MainActivity;
+
+/**
+ * Created by ken13 on 2017-11-29.
+ */
+
+public class User{
+    private final static String USERTAG="debug from user.class:";
+    private FirebaseAuth mAuth;
+    private Activity activity;
+    public static User instance;
+    private boolean accessibility;
+    private User(Activity activity){
+        mAuth = FirebaseAuth.getInstance();
+        Log.i(USERTAG,"어떠: "+ mAuth.toString());
+        this.activity=activity;
+
+    }
+
+    public static User getInstance(Activity activity){
+        if(instance==null)
+            instance =new User(activity);
+        return instance;
+
+    }
+
+    public boolean logIn(String email, String password){
+        Log.i("kkkk","login");
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(activity, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(USERTAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Log.i("kkkk","login:"+user.getEmail());
+                            accessibility=true;
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(USERTAG, "signInWithEmail:failure", task.getException());
+                            Log.i("kkkk","login failed");
+                            accessibility=false;
+                        }
+
+                        // ...
+                    }
+                });
+
+        return accessibility;
+    }
+
+
+    public boolean signUp(String email, String password){
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(activity, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(USERTAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            accessibility=true;
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(USERTAG, "createUserWithEmail:failure", task.getException());
+
+                            accessibility=false;
+                        }
+
+                    }
+                });
+
+        return accessibility;
+    }
+
+
+    public Boolean getAccessibility(){
+        return accessibility;
+    }
+}
