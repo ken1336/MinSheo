4f0620e1c51c940d10342c686bf6398e91d93ed9
seh9306 <qkr047845%>
2017-12-02 19:29:38 +0900

    첫번째 commit test

--- app/src/main/java/applicaton/android/com/sehonmin/MainActivity.java
+++ app/src/main/java/applicaton/android/com/sehonmin/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
     private EditText edit_password;
     private Button signup_btn;
     private User user;
-
+//commit test
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
